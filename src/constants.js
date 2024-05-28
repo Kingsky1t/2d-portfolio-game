af7ceeeca57058af87f736ff226c4571656b704b
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2024-05-28 10:49:16 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+    pc:"this is my pc",
+    "cs-degree": "this is my cs degree",
+    "sofa-table":"this is a sofa",
+    tv:"this is my tv",
+    bed:"this is my bed",
+    resume:"this is my resume",
+    projects:"these are my projects",
+    library:"this is a library",
+    exit:"exit! bye",
+}

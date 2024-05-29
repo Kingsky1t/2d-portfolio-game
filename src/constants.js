8ef82c3c1e9aa416042a040089bccd8f54b676f3
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2024-05-29 15:41:24 +0530

    minor changes

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,13 @@
 export const scaleFactor = 4;
 export const dialogueData = {
-    pc:"this is my pc",
-    "cs-degree": "this is my cs degree",
-    "sofa-table":"this is a sofa",
-    tv:"this is my tv",
-    bed:"this is my bed",
-    resume:"this is my resume",
-    projects:"these are my projects",
-    library:"this is a library",
-    exit:"exit! bye",
-}
+  pc: "This is my PC. I mostly work on JavaScript but I also know Java and Python. I am mostly interested in development work.",
+  "cs-degree":
+    "This is my cs degree. I hung it on my wall because I am proud of it.",
+  "sofa-table": "That's my sofa. I relax and watch Youtube here.",
+  tv: `Recently I have been watching a lot of Chess videos. Maybe I can take it up as a hobby. <a href="https://www.youtube.com/@GothamChess" target=_blank>Gotham Chess</a> helps me a lot to learn this game.`,
+  bed: "Now this is where I sleep. Great ideas come to my mind here",
+  resume: `You can see my resume <a href="https://drive.google.com/file/d/1xuSLcDLvYi4YPBDyuTyyQzMNjx9aE_Pl/view?usp=sharing" target=_blank>here</a>`,
+  projects: `Go check out all my <a href="https://shaunak.pages.dev/projects" target=_blank>projects</a> `,
+  library: "There are a lot of programming books on my shelves.",
+  exit: "Bye!! 🖐",
+};
